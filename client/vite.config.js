7f2aed6d2b2964fe0b8bfe6fa3541d43e37547c5
Jânio Muianga <janiodrey@gmail.com>
2023-03-20 17:14:44 +0200

    idst path

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 4000,
-  },
-  plugins: [react()],
-});
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    build: {
+      outDir: process.env.VITE_APP_DIST,
+      emptyOutDir: true
+    },
+    base: process.env.VITE_API_URL,
+    plugins: [react()]
+  })
+};
